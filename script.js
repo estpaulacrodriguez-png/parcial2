79fb5202d5f760d5108a5140058115bafc0d14cf
Paula Rodriguez <est.paulac.rodriguez@unimilitar.edu.co>
2026-04-24 12:49:34 -0500

    Implementación de Bresseham

--- script.js
+++ script.js
@@ -11,6 +11,38 @@ const H = canvas.height;
 // PIXEL (ÚNICO MÉTODO DE DIBUJO)
 // ===============================
 function drawPixel(ctx, x, y, color = "#0000FF") { // azul
-  ctx.fillStyle = color ;
+  ctx.fillStyle = color  ;
   ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+// ===============================
+// BRESENHAM (LÍNEAS)
+// ===============================
+function bresenhamLine(x0, y0, x1, y1) {
+
+  let dx = Math.abs(x1 - x0);
+  let dy = Math.abs(y1 - y0);
+
+  let sx = (x0 < x1) ? 1 : -1;
+  let sy = (y0 < y1) ? 1 : -1;
+
+  let err = dx - dy;
+
+  while (true) {
+
+    drawPixel(ctx, x0, y0);
+
+    if (x0 === x1 && y0 === y1) break;
+
+    let e2 = 2 * err;
+
+    if (e2 > -dy) {
+      err -= dy;
+      x0 += sx;
+    }
+
+    if (e2 < dx) {
+      err += dx;
+     
+    }
+  }
 }
